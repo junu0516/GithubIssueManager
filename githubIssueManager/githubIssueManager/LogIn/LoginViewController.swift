09e0e546298d4d72e045f2f87f7149b8cbe5a1ff
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-18 12:36:06 +0900

    [#4] Chore: iOS 버전 14.0으로 격하, 로그인 버튼 설정 맞춰서 수정

--- githubIssueManager/githubIssueManager/LogIn/LoginViewController.swift
+++ githubIssueManager/githubIssueManager/LogIn/LoginViewController.swift
@@ -4,15 +4,14 @@ import SnapKit
 final class LoginViewController: UIViewController {
     
     private let githubLoginButton: UIButton = {
-        var config = UIButton.Configuration.filled()
-        config.baseBackgroundColor = .black
-        config.image = UIImage(named: "icon_github")
-        config.imagePadding = 8
-        config.imagePlacement = .leading
-        config.title = "GitHub 계정으로 로그인"
-        config.cornerStyle = .medium
-        
-        let button = UIButton(configuration: config)
+        let button = UIButton()
+        button.setTitle(" Github 계정으로 로그인", for: .normal)
+        button.backgroundColor = .black
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.setImage(UIImage(named: "icon_github"), for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
         return button
     }()
 
@@ -25,7 +24,7 @@ final class LoginViewController: UIViewController {
         view.addSubview(githubLoginButton)
         githubLoginButton.snp.makeConstraints {
             $0.centerY.equalToSuperview()
-            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.leading.trailing.equalToSuperview().inset(15)
             $0.height.equalToSuperview().multipliedBy(0.1)
         }
     }
