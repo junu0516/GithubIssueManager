f29be0b73f7af9bfbfca6dd166b2b05f2e07ff5e
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-25 14:35:04 +0900

    [#14] Feat: 이슈 추가 화면 구현(IssueInsertViewController)

--- githubIssueManager/githubIssueManager/Main/Issue/IssueInsert/IssueInsertViewController.swift
+++ githubIssueManager/githubIssueManager/Main/Issue/IssueInsert/IssueInsertViewController.swift
@@ -3,6 +3,79 @@ import SnapKit
 
 final class IssueInsertViewController: UIViewController {
     
+    private let saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("저장", for: .normal)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.titleLabel?.textAlignment = .left
+        return button
+    }()
+    
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.systemFont(ofSize: 18)
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.layer.borderWidth = 0.5
+        return textView
+    }()
+    
+    private let segmentView: UISegmentedControl = {
+        let segmentView = UISegmentedControl(items: ["마크다운","미리보기"])
+        segmentView.selectedSegmentIndex = 0
+        return segmentView
+    }()
+    
+    private let titleView: InsertField = {
+        let insertField = InsertField()
+        insertField.title = "제목"
+        insertField.placeHolder = "제목을 입력하세요"
+        insertField.backgroundColor = .white
+        return insertField
+    }()
+    
+    private let insertForm: IssueInsertForm = {
+        let insertForm = IssueInsertForm()
+        insertForm.backgroundColor = .systemBackground
+        return insertForm
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setAttributes()
+        addViews()
+    }
+    
+    private func addViews() {
+        
+        view.addSubview(titleView)
+        titleView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.height.equalToSuperview().multipliedBy(0.07)
+        }
+        
+        view.addSubview(textView)
+        textView.snp.makeConstraints {
+            $0.top.equalTo(titleView.snp.bottom)
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.height.equalToSuperview().multipliedBy(0.53)
+        }
+        
+        view.addSubview(insertForm)
+        insertForm.snp.makeConstraints {
+            $0.top.equalTo(textView.snp.bottom)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(15)
+        }
+    }
+    
+    private func setAttributes() {
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveButton)
+        navigationItem.titleView = segmentView
+    }
 }
 
 
