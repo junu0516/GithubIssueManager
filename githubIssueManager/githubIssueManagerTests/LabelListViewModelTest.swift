f18b5d09f29ef3e89459b8aa440a01b27d22dc5d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-27 20:27:51 +0900

    [#16] Test: 라벨 리스트 불러오기 단위테스트 추가

---
+++ githubIssueManager/githubIssueManagerTests/LabelListViewModelTest.swift
@@ -0,0 +1,41 @@
+import XCTest
+@testable import githubIssueManager
+
+class LabelListViewModelTest: XCTestCase {
+
+    private var networkManager: MockNetworkManager!
+    private var viewModel: LabelListViewModel!
+
+    
+    override func setUpWithError() throws {
+        self.networkManager = MockNetworkManager()
+        
+        let repository = LabelListRepository(networkManager: networkManager)
+        self.viewModel = LabelListViewModel(repository: repository)
+    }
+    
+    func test_loading_mock_data() {
+        networkManager.loadMockData(mockData: .labels)
+        XCTAssertNotNil(networkManager.mockResponseData)
+    }
+    
+    func test_fetching_labels_success() throws {
+        networkManager.shouldFail = false
+        
+        XCTAssertNil(viewModel.output.labelCellModels.value)
+        
+        viewModel.input.labelListRequested.value = true
+        let labels = try XCTUnwrap(viewModel.output.labelCellModels.value)
+        XCTAssertGreaterThan(labels.count, 0)
+    }
+    
+    func test_fetching_labels_failure() throws {
+        networkManager.shouldFail = true
+        
+        XCTAssertNil(viewModel.output.labelCellModels.value)
+        
+        viewModel.input.labelListRequested.value = true
+        XCTAssertNil(viewModel.output.labelCellModels.value)
+    }
+
+}
