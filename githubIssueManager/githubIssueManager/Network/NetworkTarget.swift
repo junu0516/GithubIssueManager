70a6d68ce1ffdafcc23ddee5f6ca8b349cf0522b
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-07-10 03:45:45 +0900

    [#18] Refactor: NetworkTarget 실패가능한 생성자 제거, URLComponents 타입 인스턴스 생성 책임 분리

--- githubIssueManager/githubIssueManager/Network/NetworkTarget.swift
+++ githubIssueManager/githubIssueManager/Network/NetworkTarget.swift
@@ -9,21 +9,16 @@ enum ParameterType {
 struct NetworkTarget {
     
     let path: Path
-    let url: URLComponents
     let method: HttpMethod
     let headers: [String:String]
     let paramterType: ParameterType
     
-    init?(path: Path,
+    init(path: Path,
           method: HttpMethod = HttpMethod.get,
           paramteterType: ParameterType,
           headers: [String:String] = ["content-type":"applicatin/json"]) {
-        guard let url = URLComponents(string: "\(path)") else {
-            return nil
-        }
         
         self.path = path
-        self.url = url
         self.method = method
         self.headers = headers
         self.paramterType = paramteterType
