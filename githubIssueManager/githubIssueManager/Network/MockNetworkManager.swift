dce1987d3443f5d04f71f42d7ab6e01976793974
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-07-10 03:33:33 +0900

    [#18] Refactor: MockNetworkManager 개선, 테스트코드 수정

--- githubIssueManager/githubIssueManager/Network/MockNetworkManager.swift
+++ githubIssueManager/githubIssueManager/Network/MockNetworkManager.swift
@@ -2,6 +2,73 @@ import Foundation
 
 final class MockNetworkManager: NetworkManagable {
     
+    //Mock 객체 내부에서 사용할 사용자 정의 에러 열거형
+    enum TestError: Error, LocalizedError {
+        case mockDataNotLoaded
+        case responseError
+        
+        var errorDescription: String? {
+            switch self {
+            case .mockDataNotLoaded:
+                return "TestError.\(self) : Mock Data Not Loaded"
+            case .responseError:
+                return "TestError.\(self) : Response Error"
+            }
+        }
+    }
+    
+    //미리 성공, 실패 여부를 결정
+    var shouldFail: Bool = true
+    
+    func request<T: Encodable>(target: NetworkTarget, parameters: T?, completion: @escaping (Result<Data, Error>) -> Void) {
+        
+        switch shouldFail {
+        case false:
+            //성공한 응답가지고 클로저 호출
+            do {
+                let mockResponseData = try getMockDatWithPath(path: target.path)
+                completion(.success(mockResponseData))
+            } catch {
+                Log.error(error.localizedDescription)
+            }
+        case true:
+            //실패한 응답 가지고 클로저 호출
+            completion(.failure(TestError.responseError))
+        }
+    }
+    
+    private func getMockDatWithPath(path: Path) throws -> Data {
+        switch path {
+        case .githubLogin(_):
+            return Data()
+        case .githubAccessToken:
+            return try loadMockData(mockData: .authentication)
+        case .issues(_,_):
+            return try loadMockData(mockData: .issues)
+        case .milestones(_,_):
+            return try loadMockData(mockData: .milestones)
+        case .labels(_,_):
+            return try loadMockData(mockData: .labels)
+        case .assignees(_,_):
+            return try loadMockData(mockData: .assignees)
+        }
+    }
+    
+    //필요 시 로컬에 미리 저장된 json 데이터를 가지고옴
+    private func loadMockData(mockData: MockData) throws -> Data {
+        guard let path = Bundle.main.url(forResource: mockData.fileName, withExtension: "json") else {
+            throw TestError.mockDataNotLoaded
+        }
+        guard let data: Data = try? Data(contentsOf: path) else {
+            throw TestError.mockDataNotLoaded
+        }
+
+        return data
+    }
+}
+
+extension MockNetworkManager {
+    
     enum MockData {
         case issues
         case authentication
@@ -22,64 +89,6 @@ final class MockNetworkManager: NetworkManagable {
             case .assignees:
                 return "assignees"
             }
-        }
-    }
-    
-    //Mock 객체 내부에서 사용할 사용자 정의 에러 열거형
-    enum TestError: Error, CustomStringConvertible {
-        case mockDataNotLoaded
-        case responseError
-        
-        var description: String {
-            switch self {
-            case .mockDataNotLoaded:
-                return "Mock Data Not Loaded"
-            case .responseError:
-                return "Response Error"
-            }
-        }
-    }
-    
-    //미리 성공, 실패 여부를 결정
-    var shouldFail: Bool = true
-    var mockResponseData: Data?
-    
-    //필요 시 로컬에 미리 저장된 json 데이터를 가지고옴
-    func loadMockData(mockData: MockData) {
-        guard let path = Bundle.main.url(forResource: mockData.fileName, withExtension: "json") else { return }
-        guard let data: Data = try? Data(contentsOf: path) else { return }
-        self.mockResponseData = data
-    }
-    
-    func request<T: Encodable>(target: NetworkTarget, parameters: T?, completion: @escaping (Result<Data, Error>) -> Void) {
-        
-        bindPathToMockData(path: target.path)
-        
-        switch shouldFail {
-        case false:
-            //성공한 응답가지고 클로저 호출
-            completion(.success(mockResponseData ?? Data()))
-        case true:
-            //실패한 응답 가지고 클로저 호출
-            completion(.failure(TestError.responseError))
-        }
-        
-    }
-    
-    private func bindPathToMockData(path: Path) {
-        switch path {
-        case .githubLogin(_):
-            return
-        case .githubAccessToken:
-            loadMockData(mockData: .authentication)
-        case .issues(_,_):
-            loadMockData(mockData: .issues)
-        case .milestones(_,_):
-            loadMockData(mockData: .milestones)
-        case .labels(_,_):
-            loadMockData(mockData: .labels)
-        case .assignees(_,_):
-            loadMockData(mockData: .assignees)
         }
     }
 }
