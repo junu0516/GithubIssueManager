d525a892b8def5984fdb91b1e95301234818e564
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-09-09 01:06:52 +0900

    [#33] feat: TableViewCell에 selectionHandler 속성, setSelected 함수 오버라이딩 추가

--- githubIssueManager/githubIssueManager/Common/ViewComponents/TableViewCell.swift
+++ githubIssueManager/githubIssueManager/Common/ViewComponents/TableViewCell.swift
@@ -3,7 +3,9 @@ import UIKit
 class TableViewCell: UITableViewCell {
 
     static var identifier: String { .init(describing: self) }
-
+    
+    var selectionHandler: (() -> Void) = {  }
+        
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addViews()
@@ -13,6 +15,11 @@ class TableViewCell: UITableViewCell {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        selectionHandler()
     }
 
     override func prepareForReuse() {
